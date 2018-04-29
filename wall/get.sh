f775df37d79e616e71db50caac26d9d930583478
Alex Aubuchon <alex@alxe.co>
2018-04-29 21:14:25 +0200

    Wallpaper updates

---
+++ wall/get.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# From https://maxbeechcreative.deviantart.com/art/Field-of-Fire-696546620
+# ©2017-2018 maxbeechcreative
+curl -o wall.jpg https://drive.google.com/uc?export=view&id=1L8XDyNaM3i6vvUZgLXm4ERf95RypbfQt
